500326348b67e9c3bafd483d23028b7244386ed4
Geoff Adebonojo <geoffadebonojo@Geoffs-MacBook-Pro.local>
2023-02-23 22:15:15 -0700

    i think I'm on the right track

--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -8,13 +8,42 @@ module Types
     
     field :link, [Types::D3::LinkType], null: true do
       description "Find a link by id"
-      argument :movie_id, ID
+      argument :movie_ids, [ID]
+      argument :person_ids, [ID]
     end
 
-    def link(movie_id)
-      Link.where(movie_id)
+    def link(args)
+      set_index = node_index_for_network(args)
+
+      Link.where(person_id: args[:person_ids])
+      .or(Link.where(movie_id: args[:movie_ids])
+      ).map do |x|
+        j = set_index.find_index("person-#{x.person_id}")
+        k = set_index.find_index("movie-#{x.movie_id}")
+        next if j.nil? || k.nil?
+
+        {
+          source: j,
+          target: k,
+          roles: x.roles.reject(&:empty?)
+        }
+      end.compact
     end
 
+    def node_index_for_network(ids=[])
+      selected_nodes(ids).map { |x| 
+        "#{x.media_type}-#{x.id}"
+      }
+    end
+    
+    def selected_nodes(ids)
+      movie_ids = ids[:movie_ids]
+      person_ids = ids[:person_ids]
+      return [
+        Person.where(id: person_ids),
+        Movie.where(id: movie_ids)
+      ].flatten
+    end
 
     def links
       pids = Person.find(500).id
