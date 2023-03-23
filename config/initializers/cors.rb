157ba485edd494ed6982f1c4af311b21b9a06748
geoffadebonojo <geoffrey.adebonojo@gmail.com>
2023-03-23 14:38:30 -0600

    fix cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -17,7 +17,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins "https://filmetrix.netlify.app"
 
-    resource "/graphql",
+    resource "/graph",
       headers: :any,
       methods: [:post]
   end
