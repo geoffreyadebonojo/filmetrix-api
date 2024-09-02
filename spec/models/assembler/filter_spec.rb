c19a86f1fced09f236fc4d6438a03c8e92612aa6
geoffadebonojo <geoffrey.adebonojo@gmail.com>
2024-09-02 09:35:03 -0600

    Add specs

---
+++ spec/models/assembler/filter_spec.rb
@@ -0,0 +1,123 @@
+require 'rails_helper'
+require 'spec_helper'
+
+RSpec.describe Assembler::Filter, type: :model do
+
+  describe "filters" do
+    movie_credits = eval(File.read("spec/fixtures/filter/oblivion-credits.json").gsub("null", "nil"))
+    filter = Assembler::Filter.new(movie_credits)
+        
+    describe "by department" do
+      it "actors" do
+        actors = filter.apply("Acting")
+
+        expect(actors.count).to eq(9)
+
+        tc = actors.first 
+
+        expect(tc[:name]).to eq("Tom Cruise")
+        expect(tc[:order]).to eq(0)
+        expect(tc[:roles]).to eq(["Jack"])
+        expect(tc[:departments]).to eq(["Acting"])
+      end
+
+      it "directors" do
+        directors = filter.apply("Directing")
+
+        expect(directors.count).to eq(3)
+
+        jk = directors.first 
+
+        expect(jk[:name]).to eq("Joseph Kosinski")
+        # TODO: check that only actors have order
+        # expect(jk[:order]).to eq(0)
+        expect(jk[:roles]).to eq(["Original Story", "Director", "Producer"])
+        expect(jk[:departments]).to eq(["Writing", "Directing", "Production"])
+      end
+
+      it "producers" do
+        producers = filter.apply("Production")
+
+        expect(producers.count).to eq(59)
+
+        pc = producers.first 
+
+        expect(pc[:name]).to eq("Peter Chernin")
+        expect(pc[:roles]).to eq(["Producer"])
+        expect(pc[:departments]).to eq(["Production"])
+      end
+
+      it "does overlaps" do
+        directors = filter.apply("Directing")
+        expect(directors.map{|d|d[:name]}).to include("Joseph Kosinski")
+
+        producers = filter.apply("Production")
+        expect(producers.map{|d|d[:name]}).to include("Joseph Kosinski")
+
+        writers = filter.apply("Writing")
+        expect(writers.map{|d|d[:name]}).to include("Joseph Kosinski")
+      end
+    end
+
+    describe ".list_departments" do
+      it "correctly lists all departments" do 
+        depts = filter.list_departments
+        expected = ["Acting",
+          "Production",
+          "Art",
+          "Writing",
+          "Directing",
+          "Costume & Make-Up",
+          "Crew",
+          "Sound",
+          "Editing",
+          "Camera",
+          "Lighting",
+          "Visual Effects"]
+        expect(depts).to eq(expected)
+      end
+    end
+
+    describe ".gather( options )" do
+      it "raises ArgumentError if one of the options is not a valid department name" do
+        options = {directing: 1, acting: 3, writing: 1, fighting: 10}
+        expect { filter.gather(options) }.to raise_error(ArgumentError)
+      end
+
+      it "returns a people without passing argument" do
+        f = filter.gather
+
+        expected = [["Writing", "Directing", "Production"],
+        ["Production"],
+        ["Production"],
+        ["Writing"],
+        ["Acting"],
+        ["Acting"],
+        ["Acting"],
+        ["Acting"],
+        ["Acting"],
+        ["Acting"],
+        ["Acting"],
+        ["Acting"],
+        ["Acting"]]
+
+        expect(f.count).to eq(13)
+        # more to consider here, with the way that these are counted and duplicates collapsed. 
+        # !!May not always add up to the same total as in the options!!
+        expect(f.map{|x|x[:departments]}).to eq(expected)
+      end
+
+      it "returns the right amount of people" do
+        options = {directing: 1, acting: 3, writing: 1}
+        f = filter.gather(options)
+
+        expect(f.count).to eq(5)
+
+        expected_names = ["Joseph Kosinski", "Tom Cruise", "Morgan Freeman", "Olga Kurylenko", "Michael Arndt"]
+        expect(f.map{|x|x[:name]}).to eq(expected_names)
+      end
+    end
+
+  end
+
+end
--- spec/models/assembler/matcher_spec.rb
+++ spec/models/assembler/matcher_spec.rb
@@ -5,14 +5,14 @@ RSpec.describe Assembler::Matcher, type: :model do
 
   describe "in person-to-person matches" do
     it "doesn't find false positives" do
-      credit_list = eval(File.read("spec/tom-cruise-kevin-hart-list.json").gsub("null", "nil"))
+      credit_list = eval(File.read("spec/fixtures/matcher/tom-cruise-kevin-hart-list.json").gsub("null", "nil"))
       matcher = Assembler::Matcher.new(credit_list)
 
       expect(matcher.found_matches).to eq([])
     end
 
     it "finds overlapping entries between two people" do
-      credit_list = eval(File.read("spec/tom-cruise-brad-pitt-list.json").gsub("null", "nil"))
+      credit_list = eval(File.read("spec/fixtures/matcher/tom-cruise-brad-pitt-list.json").gsub("null", "nil"))
       matcher = Assembler::Matcher.new(credit_list)
       expected = ["movie-628", "movie-37757", "movie-126314", "movie-1113682"]
 
@@ -20,7 +20,7 @@ RSpec.describe Assembler::Matcher, type: :model do
     end
 
     it "finds overlapping entries between multiple people" do
-      credit_list = eval(File.read("spec/tom-brad-morgan.json").gsub("null", "nil"))
+      credit_list = eval(File.read("spec/fixtures/matcher/tom-brad-morgan.json").gsub("null", "nil"))
       matcher = Assembler::Matcher.new(credit_list)
       expected = ["movie-74", "movie-628", "movie-75612", "movie-37757", "movie-126314", "movie-1113682", "movie-807", "movie-1242980"]
 
@@ -30,14 +30,14 @@ RSpec.describe Assembler::Matcher, type: :model do
 
   describe "in movie-to-movie matches" do
     it "doesn't find false positives" do
-      credit_list = eval(File.read("spec/busan-mission-impossible-list.json").gsub("null", "nil"))
+      credit_list = eval(File.read("spec/fixtures/matcher/busan-mission-impossible-list.json").gsub("null", "nil"))
       matcher = Assembler::Matcher.new(credit_list)
       
       expect(matcher.found_matches).to eq([])
     end
     
     it "finds overlapping entries between two credit lists" do
-      credit_list = eval(File.read("spec/vampire-mission-impossible-list.json").gsub("null", "nil"))
+      credit_list = eval(File.read("spec/fixtures/matcher/vampire-mission-impossible-list.json").gsub("null", "nil"))
       matcher = Assembler::Matcher.new(credit_list)
 
       # more than are being shown on the graph...
@@ -47,7 +47,7 @@ RSpec.describe Assembler::Matcher, type: :model do
     end
 
     it "finds overlapping entries between multiple credit lists" do
-      credit_list = eval(File.read("spec/se7en-vampire-oblivion-list.json").gsub("null", "nil"))
+      credit_list = eval(File.read("spec/fixtures/matcher/se7en-vampire-oblivion-list.json").gsub("null", "nil"))
       matcher = Assembler::Matcher.new(credit_list)
 
       expected = ["person-192", "person-287", "person-6581", "person-7763", "person-1458993", "person-51333", "person-1691197", "person-1854468", "person-92303", "person-500", "person-14192", "person-1411066", "person-1428908", "person-1403415"]
