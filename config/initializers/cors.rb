3c865d37456236a820e36ff73fbbf2bbfd66a2a4
geoffadebonojo <geoffrey.adebonojo@gmail.com>
2025-01-27 14:27:54 -0700

    WIP

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:5173",
-            "https://filmetrix.netlify.app"
+    # origins "http://localhost:5173",
+    #         "https://filmetrix.netlify.app"
+    origins "*"
 
     resource "/graphql",
       headers: :any,
