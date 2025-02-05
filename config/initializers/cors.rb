2df92afdec521c6f7af8037f9cae3694786b5ef2
geoffadebonojo <geoffrey.adebonojo@gmail.com>
2025-02-05 14:25:18 -0700

    Add cors origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -8,8 +8,7 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     # origins "http://localhost:5173",
-    #         "https://filmetrix.netlify.app"
-    origins "*"
+    origins "https://filmetrix.netlify.app"
 
     resource "/graphql",
       headers: :any,
