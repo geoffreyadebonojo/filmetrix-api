b95badc1128ea31284849b0c9b78d90d1eeb37fc
Robert <robert@alkami.io>
2025-02-26 19:57:33 -0700

    Added Dockerfile and docker-compose for containerizing the api. Fixed bug on development.rb on cache_store. Tested application running in container locally.  Was able to Build DB, run migrations and run both db and web api service. Visiting localhost:3000 renders Rails page as expected.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -26,7 +26,7 @@ Rails.application.configure do
     }
   else
     config.action_controller.perform_caching = true
-    config.cache_store = :memory
+    config.cache_store = :memory_store
   end
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
